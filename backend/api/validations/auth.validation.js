628a59b28a143bd85a3e718d2825c1719a182868
Sajal Shrestha <sajal.shres@gmail.com>
2021-01-23 23:14:20 +0545

    package updates and auth validation fixed

--- backend/api/validations/auth.validation.js
+++ backend/api/validations/auth.validation.js
@@ -3,49 +3,49 @@ const Joi = require('joi');
 module.exports = {
   // POST /v1/auth/register
   register: {
-    body: {
+    body: Joi.object({
       email: Joi.string().email().required(),
       password: Joi.string().required().min(6).max(128),
-    },
+    }),
   },
 
   // POST /v1/auth/login
   login: {
-    body: {
+    body: Joi.object({
       email: Joi.string().email().required(),
       password: Joi.string().required().max(128),
-    },
+    }),
   },
 
   // POST /v1/auth/facebook
   // POST /v1/auth/google
   oAuth: {
-    body: {
+    body: Joi.object({
       access_token: Joi.string().required(),
-    },
+    }),
   },
 
   // POST /v1/auth/refresh
   refresh: {
-    body: {
+    body: Joi.object({
       email: Joi.string().email().required(),
       refreshToken: Joi.string().required(),
-    },
+    }),
   },
 
   // POST /v1/auth/refresh
   sendPasswordReset: {
-    body: {
+    body: Joi.object({
       email: Joi.string().email().required(),
-    },
+    }),
   },
 
   // POST /v1/auth/password-reset
   passwordReset: {
-    body: {
+    body: Joi.object({
       email: Joi.string().email().required(),
       password: Joi.string().required().min(6).max(128),
       resetToken: Joi.string().required(),
-    },
+    }),
   },
 };
