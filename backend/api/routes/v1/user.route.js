9adc287590682b6baed537d547c141f3ac24b46a
Sajal Shrestha <sajal.shres@gmail.com>
2021-01-24 11:18:32 +0545

    updated maxlen rule

--- backend/api/routes/v1/user.route.js
+++ backend/api/routes/v1/user.route.js
@@ -2,7 +2,12 @@ const express = require('express');
 const { validate } = require('express-validation');
 const controller = require('../../controllers/user.controller');
 const { authorize, ADMIN, LOGGED_USER } = require('../../middlewares/auth');
-const { listUsers, createUser, replaceUser, updateUser } = require('../../validations/user.validation');
+const {
+  listUsers,
+  createUser,
+  replaceUser,
+  updateUser,
+} = require('../../validations/user.validation');
 
 const router = express.Router();
 
